5e07169bf7cbb7948a8aa457756a62435adb1ef9
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-16 23:16:27 +0500

    Add files via upload

--- reactkashafapp/src/component/PortFolio/PortFolio.jsx
+++ reactkashafapp/src/component/PortFolio/PortFolio.jsx
@@ -1,1 +1,57 @@
+import React from 'react'
+import './PortFolio.css';
 
+import HOC from  "../../img/hoc.png";
+import MusicApp from "../../img/musicapp.png";
+import Sidebar from "../../img/sidebar.png";
+import Ecommerce from "../../img/ecommerce.png";
+
+
+import 'swiper/css';
+//we are using here swiper library
+//npm add swiper
+import {Swiper,SwiperSlide} from 'swiper/react';
+
+
+const PortFolio = () => {
+  return (
+    <div className="portfolio">
+{/* heading */}
+<span>Recent Projects</span>
+<span>PortFolio</span>
+
+
+{/* slider */}
+{/* these space between, slideperview,gradcursor are slider parameter */}
+<Swiper
+ spaceBetween={30} 
+slidesPerView={3}
+gradCursor={true}
+className='portfolio-slider'
+>
+    <SwiperSlide>
+<img src={Sidebar} alt=""/>
+</SwiperSlide>
+
+
+<SwiperSlide>
+<img src={Ecommerce} alt=""/>
+</SwiperSlide>
+
+
+
+<SwiperSlide>
+<img src={MusicApp} alt=""/>
+</SwiperSlide>
+
+
+<SwiperSlide>
+<img src={HOC} alt=""/>
+</SwiperSlide>
+
+</Swiper>
+    </div>
+  )
+}
+
+export default PortFolio;
