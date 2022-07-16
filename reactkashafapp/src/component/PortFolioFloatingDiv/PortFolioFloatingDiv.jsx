2e97139ee7853bca8838b908a46c1be597a75dee
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-16 23:38:20 +0500

    Add files via upload

--- reactkashafapp/src/component/PortFolioFloatingDiv/PortFolioFloatingDiv.jsx
+++ reactkashafapp/src/component/PortFolioFloatingDiv/PortFolioFloatingDiv.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
 
+import './PortFolioFloatingDiv.css';
+const PortFolioFloatingDiv = ({image,txt}) => {
+  return (
+    <div className='floatingdiv' style={{background:"pink"}}>
+        <img src={image} alt="img her"  />
+        <span>{txt}</span>
+    </div>
+  )
+}
+
+export default PortFolioFloatingDiv
