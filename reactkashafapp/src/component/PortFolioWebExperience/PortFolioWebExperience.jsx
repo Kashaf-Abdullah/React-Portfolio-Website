ea184e8d29ede24e5d9426250b54abc38b27f3ad
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-16 23:25:11 +0500

    Add files via upload

--- reactkashafapp/src/component/PortFolioWebExperience/PortFolioWebExperience.jsx
+++ reactkashafapp/src/component/PortFolioWebExperience/PortFolioWebExperience.jsx
@@ -1,1 +1,23 @@
+import React from 'react'
+import PortFolioWebExperienceDiv from '../PortFolioWebExperienceDiv/PortFolioWebExperienceDiv';
+import './PortFolioWebExperience.css'
+const PortFolioWebExperience = () => {
+  return (
 
+  <>
+<div className="experience" id='Experience'>
+  <span className='expHeading'>EXPERIENCE</span>
+  <div className="exp" style={{display:"flex" , gap:"50px" ,marginTop:"84px" ,justifyContent:"center", flexDirection:"row",flex:"Wrap"}}> 
+   <PortFolioWebExperienceDiv exp="5+"  txt="Projects"/>
+  <PortFolioWebExperienceDiv exp="1+"  txt=" Web developement"/>
+   <PortFolioWebExperienceDiv exp="2+"  txt="Coding Experience"/>
+<PortFolioWebExperienceDiv exp="1+"  txt="Freelancing Experience"/>
+</div>
+
+
+</div>
+  </>
+  )
+}
+
+export default PortFolioWebExperience
