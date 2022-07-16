c6110061c26cb52bf2dd20712dd270d001477bae
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-16 23:36:51 +0500

    Add files via upload

--- reactkashafapp/src/component/PortFolioFooter/PortFolioFooter.jsx
+++ reactkashafapp/src/component/PortFolioFooter/PortFolioFooter.jsx
@@ -1,1 +1,40 @@
+import React from 'react'
 
+
+import './PortFolioFooter.css'
+
+import Wave from '../../img/wave.png';
+
+
+import Insta from '@iconscout/react-unicons/icons/uil-instagram';
+import Facebook from '@iconscout/react-unicons/icons/uil-facebook';
+import Github from '@iconscout/react-unicons/icons/uil-github';
+
+const PortFolioFooter = () => {
+  return (
+    <div className="footer">
+    <img src={Wave} alt="" style={{width:'100%'}}/>
+ 
+ <div className="f-content">
+
+    {/* use library for ucon 
+    search react unicons icon on google */}
+
+{/* 
+npm add @iconscout/react-unicons */}
+
+
+<span>kashafabdullah@gmail.com</span>
+<div className="f-icons">
+    <Insta color='White' size='3rem'/>
+    <Facebook color='White' size='3rem'/>
+    <Github color='White' size='3rem'/>
+
+</div>
+
+ </div>
+ </div>
+    )
+}
+
+export default PortFolioFooter
