852dc33234cbc5a9630c85acf80d914cb7e8f1e3
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-16 23:56:42 +0500

    Add files via upload

---
+++ reactkashafapp/src/App.js
@@ -0,0 +1,182 @@
+import React from "react";
+import "./App.css";
+// import Kaamseyfooter from "./component/Kaamseyfooter";
+// import Website1 from "./component/Website1";
+// import Header  from "./component/Header";
+// import Social from "./component/Social";
+// import Profile from "./component/Profile";
+// import PortFolio from "./component/PortFolio";
+// import Propslearn from "./component/Propslearn";
+ 
+
+// import Web2 from "./component/Web2/Web2";
+//  import Web2Intro from "./component/Web2Intro/Web2Intro";
+//  import Web2Service from "./component/Web2Service/Web2Service";
+//  import Web2Experience from "./component/Web2Experience/Web2Experience";
+//  import Web2Work from "./component/Web2Work/Web2Work";
+ 
+ 
+//  import Web2PortFolio from "./component/Web2PortFolio/Web2PortFolio";
+
+//  import Web2Testimonials from "./component/Web2Testimonials/Web2Testimonials";
+
+
+//  import Web2Contact from "./component/Web2Contact/Web2Contact";
+//  import Web2Footer from "./component/Web2Footer/Web2Footer";
+
+
+
+
+ // import Restaurant from "./component/Restaurant";
+// import Navbarweb from "./component/Navbarweb/Navbarweb";
+
+// import Intro from "./component/Intro/Intro";
+
+// import Services from "./component/Services/Services";
+
+// import Card from "./component/Card/Card";
+
+// import Experience from "./component/Experience/Experience";
+// import Works from "./component/Works/Works";
+// import PortFolio from './component/PortFolio/PortFolio';
+// import Testimonial from './component/Testimonials/Testimonials'
+// import Contact from './component/Contact/Contact';
+// import Footer from './component/Footer/Footer';
+
+
+
+
+import PortFolioWebNavBar from './component/PortFolioWebNavBar/PortFolioWebNavBar';
+
+import PortFolioWebIntro from './component/PortFolioWebIntro/PortFolioWebIntro';
+import PortFolioWebExperience from './component/PortFolioWebExperience/PortFolioWebExperience';
+import PortFolioWebEducation from './component/PortFolioWebEducation/PortFolioWebEducation';
+import PortFolioContactForm from './component/PortFolioContactForm/PortFolioContactForm'
+import PortFolioSkills from './component/PortFolioSkills/PortFolioSkills'
+import PortFolioWork from './component/PortFolioWork/PortFolioWork'
+import PortFolioAbout from './component/PortFolioAbout/PortFolioAbout'
+import PortFolioFooter from './component/PortFolioFooter/PortFolioFooter'
+// import PortFolioProjectLinks from './component/PortFolioProjectLinks/PortFolioProjectLinks'
+
+
+import {
+  VStack,
+
+  Flex,
+  Heading,
+
+  Spacer,
+  Text,
+  useColorMode,
+  HStack,
+  Icon, createIcon,
+  Center,
+  Stack,
+  Button,
+  Box,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Divider,
+  WrapItem,
+  Avatar,
+  Wrap,
+  Square
+} from "@chakra-ui/react";
+function App() {
+  
+  
+  return (
+   
+<>
+<h1>hello</h1>
+<PortFolioWebNavBar/>
+<PortFolioWebIntro/>
+<PortFolioWebExperience/>
+<PortFolioWebEducation/>
+<PortFolioSkills/>
+<PortFolioWork/>
+<PortFolioAbout/> 
+<PortFolioContactForm/>
+<PortFolioFooter/>
+</>
+
+
+
+
+
+// {/* <Propslearn title="Hello" content="Content 1" link="https://reactjs.org/"/>
+// <Propslearn title="World" content="Content 2" link="https://www.w3schools.com/whatis/whatis_react.asp"/>
+// <Propslearn title="kashaf" content="Content 3" link="https://en.wikipedia.org/wiki/React_(JavaScript_library)"/> */}
+// {/* <Website1></Website1> */}
+// {/* 
+// <First titlle="hello/" content="Content 1" name="aisha"></First>
+// <First title="world" content="Content 2" name="djd"></First>
+// <First title="kashaf" content="Content 3" name="ffjfjjfjf"></First>
+// </>   */}
+  
+ 
+/* <Web2/>
+<Web2Intro/>
+<Web2Service/>
+  <Web2Experience/>
+  <Web2Work/>
+<Web2PortFolio/>
+<Web2Testimonials/>
+<Web2Contact/>
+<Web2Footer/> */
+
+
+
+/* <PortFolioWebNavBar/>
+<PortFolioWebIntro/>
+<PortFolioWebExperience/>
+<PortFolioWebEducation/>
+<PortFolioContactForm/>
+<PortFolioSkills/>
+<PortFolioWork/>
+<PortFolioAbout/> 
+
+<PortFolioFooter/>*/
+/* <PortFolioProjectLinks/> */
+
+
+
+
+  /* <Navbarweb/>
+<Intro></Intro>
+<Services/>
+
+<Experience/>
+<Works/>/
+<PortFolio/>
+<Testimonial/>
+<Contact/>
+<Footer/> */
+ 
+  );
+}
+export default App;
+
+
+
+// import YourDetail from "./component/YourDetail";
+// import YourCart from "./component/YourCart";
+// import Header from "./Header";
+// import Footer1 from "./Footer1";
+// import Footer2 from "./component/Footer2";
+// import Container1 from "./component/Container1";
+// import Container2 from "./component/Container2";
+// import Container3 from "./component/Container3";
+// import Poster from "./component/Poster";
+// import FAQ from "./component/FAQ";
+// import Login from "./component/Login";
+
+
+
+
+// <PortFolio></PortFolio>
+//  <Header></Header>
+//  <Social></Social>
+//  <Profile></Profile>
