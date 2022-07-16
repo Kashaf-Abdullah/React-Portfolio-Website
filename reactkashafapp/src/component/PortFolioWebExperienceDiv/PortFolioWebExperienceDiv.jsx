9b876861a80447c73a176813634ccddf4e027132
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-07-16 23:39:29 +0500

    Add files via upload

--- reactkashafapp/src/component/PortFolioWebExperienceDiv/PortFolioWebExperienceDiv.jsx
+++ reactkashafapp/src/component/PortFolioWebExperienceDiv/PortFolioWebExperienceDiv.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import './PortFolioWebExperienceDiv.css';
 
+const PortFolioWebExperienceDiv = ({exp,txt}) => {
+  return (
+    <div className="achievement">
+<div className="circle">
+    <span>{exp}</span>
+    <span>{txt}</span>
+
+
+</div>
+  
+    
+</div>
+
+    
+  )
+}
+
+export default PortFolioWebExperienceDiv
